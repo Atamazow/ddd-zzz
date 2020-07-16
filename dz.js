df18661f47cc40b921c4889b06f529481d838163
atamazow <atamazov99@,mail.ru>
2020-07-16 19:24:19 +0300

    добавил DZ7/2

---
+++ dz.js
@@ -0,0 +1,33 @@
+ 
+  
+  function createNote() {
+    //какой-то код
+  }
+  
+  const handleClick = function() {}
+  
+  let setBought = function() {
+    return [];
+  }
+  
+ 
+  
+  let handleSubmit = function submit() {}
+  
+  const setCounter = function() {
+    return getMoney();
+  }
+  
+  function getMoney() {
+    return 1000;
+  }
+
+
+  (function() {
+      //some code
+    })();
+    
+    
+(function mySuperFunction() {
+  //super code
+ })();
